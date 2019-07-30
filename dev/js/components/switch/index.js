1bed8a60d0799f7f8589eb0206537a0d9003225c
erinithma <erinithma@gmail.com>
2019-07-30 22:15:51 +0300

    vk and traning menu have been fixed

--- dev/js/components/switch/index.js
+++ dev/js/components/switch/index.js
@@ -4,18 +4,38 @@ import { smWidth, mdWidth, lgWidth, xlWidth } from '../../common';
 import { connect } from 'react-redux';
 
 class Menu extends React.Component {
+
+    componentDidMount() {
+        let s = document.createElement("script");
+        s.textContent = `VK.Widgets.Group("vk_groups", {mode: 3}, 171372561);`;
+        document.body.appendChild(s);
+    }
+
     render() {
         const {className, size} = this.props;
 
         return (
             size === "xl+" || size === "xl" || size === "lg" ? 
-                <div className={`list ${className}`}>
-                    <a onClick={(e) => e.preventDefault()} href="/тренажер/игра">Игра</a>
-                    <a onClick={(e) => e.preventDefault()} href="/тренажер/мажор-минор">Мажор / минор</a>
-                    <a onClick={(e) => e.preventDefault()} href="/тренажер/ноты">Ноты</a>
-                    <a onClick={(e) => e.preventDefault()} href="/тренажер/интервалы">Интервалы</a>
-                    <a onClick={(e) => e.preventDefault()} href="/тренажер/трезвучия">Трезвучия</a>
-                    <a onClick={(e) => e.preventDefault()} href="/тренажер/диктант">Диктант</a>
+                <div className={className}>
+                    <div className={`list`}>
+                        <a onClick={(e) => e.preventDefault()} href="/тренажер/игра">Игра</a>
+                        <a onClick={(e) => e.preventDefault()} href="/тренажер/мажор-минор">Мажор / минор</a>
+                        <a onClick={(e) => e.preventDefault()} href="/тренажер/ноты">Ноты</a>
+                        <a onClick={(e) => e.preventDefault()} href="/тренажер/интервалы">Интервалы</a>
+                        <a onClick={(e) => e.preventDefault()} href="/тренажер/трезвучия">Трезвучия</a>
+                        <a onClick={(e) => e.preventDefault()} href="/тренажер/диктант">Диктант</a>
+                    </div> 
+                    <br/>
+                    <br/>
+                    <div className={`list`}>
+                        <a href="/donation">Помощь сайту</a>
+                        <a href="/книги">Онлайн книги</a>
+                    </div> 
+                    <br/>
+                    <br/>
+                    <div id="vk">
+                        <div id="vk_groups"></div>
+                    </div>
                 </div>
             :
                 ''
@@ -47,5 +67,9 @@ export default styled(ConnectedMenu)`
     @media (max-width: ${mdWidth-1}px){
         top: 180px;
         right: 0;
+    }
+
+    .list {
+        width: 100%;
     }
 `;
