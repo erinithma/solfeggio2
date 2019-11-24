06c8c7c42dd82df89abe78d214e9ce7779ae0ee6
erinithma <erinithma@gmail.com>
2019-11-24 20:17:24 +0300

    add menu js

---
+++ dev/js/menu.js
@@ -0,0 +1,19 @@
+import $ from 'jquery';
+
+$("body").on("click", ".menu", function (e) {
+    e.preventDefault();
+    e.stopPropagation();
+
+    if ($(".menu-links").attr("data-show") === "show")
+        $(".menu-links").attr("data-show", "hide");
+    else
+        $(".menu-links").attr("data-show", "show");
+});
+
+$("body").on("click", function (e) {
+    if (!e.target.closest(".menu-links") && !e.target.closest(".menu")) {
+        if ($(".menu-links").attr("data-show") === "show")
+            e.preventDefault();
+        $(".menu-links").attr("data-show", "hide");
+    }
+});
